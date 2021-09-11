764d1070d286806fd39f7677356bc37e2f664cd3
Caner Taşan <caner.tasan@parasut.com>
2021-09-11 16:54:35 +0300

    fix verfiy auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
-class ApplicationController < ActionController::API
-  skip_before_action :verify_authenticity_token
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
 
   def current_user
     @current_user ||= User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
